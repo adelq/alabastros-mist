7da52f00976cca436af6c0441fd521178111770e
Adel Qalieh <aqalieh95@gmail.com>
2012-11-10 19:33:13 -0500

    Add bottom information bar

--- index.php
+++ index.php
@@ -31,80 +31,45 @@
   
 <div id="main-content">
 	<div class="four-columns">
-		<?php query_posts( array( 'post_type' => 'homepage', 'tags' => 'minipost', 'showposts' => 4 ) ); 
-		if ( have_posts() ) : while ( have_posts() ) : the_post();?>
-		<?php $minilink = get_post_meta($post->ID, 'dbt_minilink', true);
-		$al_color = get_option('al_color_scheme');
-		?>
+		<div class="col">
+			<a href="/what-is-mist/the-basics/">
+			<img class="col-icon" src="http://getmistified.com/houston/wp-content/uploads/2011/02/info.png" alt="About us"/>
+
+			</a>
+			<h3><a href="/what-is-mist/the-basics/">About us</a></h3>
+			<div class="hline"></div>
+			<div>
+			<p>Learn more about what MIST is and how it works. You’ll get a rundown of all the ins and outs of everything you need to know!</p>
+			</div>
+		</div><!-- end of col -->
 
 		<div class="col">
-			
-			<?php if($minilink !== '') { ?>
-				<a href="<?php echo $minilink; ?>">
-				<?php if (has_post_thumbnail()) { ?>
-				<img class="col-icon" src="<?php echo thumb_url(); ?>" alt="<?php the_title(); ?>"/>
-				<?php } else {} ?>
-				
-				</a>
-				<h3><a href="<?php echo $minilink; ?>"><?php the_title(); ?></a></h3>
-			<?php } else { ?>
-				
-				<?php if (has_post_thumbnail()) { ?>
-				<img class="col-icon" src="<?php echo thumb_url(); ?>" alt="<?php the_title(); ?>"/>
-				<?php } else {} ?>
-				
-				
-				<h3><?php the_title(); ?></h3>
-			<?php }?>
+			<a href="/competitions/theme/">
+			<img class="col-icon" src="/wp-content/uploads/2012/11/40_48x48.png" alt="2012 Theme!"/>
+			</a>
+			<h3><a href="/competitions/theme/">2012 Theme!</a></h3>
 			<div class="hline"></div>
-			<?php the_content(); ?>
+			<p>At last! The much awaited theme is&#8230;</p>
+			<p><strong>Family: Reconnecting Our Hearts to Home</strong></p>
 		</div><!-- end of col -->
-		<?php endwhile; endif; ?>
-	</div><!-- end of four-columns -->	
-	
-	<div class="shadow-sep margin0"></div><!-- end of shadow-sep -->
-	
-		<div class="aboutus">
-			<?php query_posts( array( 'post_type' => 'homepage', 'post_type' => 'homepage', 'tags' => 'intro', 'showposts' => 1));
-			if ( have_posts() ) : while ( have_posts() ) : the_post();?>
-			<h3><?php the_title(); ?></h3>
-			<?php if($al_color == 'light') { ?>
-				<div class="hline"></div>
-			<?php } ?>
-			
-			<?php the_content(); ?>
-			<?php endwhile; endif; ?> 
-			
-		</div><!-- end of aboutus -->
-		
-		<div class="fromtheblog">
-			<?php 
-			if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Homepage") ) : ?>
-		
-			<h3>From the Blog</h3>
-			<?php if($al_color == 'light') { ?>
-				<div class="hline"></div>
-			<?php } ?>
-			<ul>
-				<?php query_posts('showposts=3'); ?>
-				<?php while (have_posts()) : the_post(); 
-				$preview = get_post_meta($post->ID, "preview", $single = true);?>
-				        
-				<li>
-					<a href="<?php the_permalink(); ?>">
-						<?php if (has_post_thumbnail()) { ?>
-						<img src="<?php bloginfo('template_directory'); ?>/js/timthumb.php?src=<?php echo thumb_url(); ?>&w=75&h=71&q=95&zc=1" alt="<?php the_title_attribute(); ?>"/>
 
-						<?php } elseif (catch_that_image()) { ?>
-						<img src="<?php bloginfo('template_directory'); ?>/js/timthumb.php?src=<?php echo catch_that_image() ?>&w=75&h=71&q=95&zc=1" alt="<?php the_title_attribute(); ?>"/>
-						<?php } else { ?><?php } ?>
-						<?php the_title(); ?>
-					</a>
-					<?php wpe_excerpt('wpe_excerptlength_homelatest', 'wpe_excerptmore2'); ?> 
-				</li>
-				<?php endwhile; ?>
-			</ul>
-		<?php endif; ?>
-		</div><!-- end of fromtheblog -->
-</div><!-- end of main-content -->
+		<div class="col">
+			<a href="/what-is-mist/donate/">
+			<img class="col-icon" src="http://getmistified.com/houston/wp-content/uploads/2011/02/volunteer.png" alt="Get Involved With Us!"/>
+			</a>
+			<h3><a href="/what-is-mist/donate/">Donate</a></h3>
+			<div class="hline"></div>
+			<p>Want to help out with MIST Detroit? Please donate in the link above, or become a volunteer!</p>
+		</div><!-- end of col -->
+
+		<div class="col">
+			<a href="/registration/form-a-team/">
+			<img class="col-icon" src="http://getmistified.com/houston/wp-content/uploads/2011/02/youtube.png" alt="Compete"/>
+			</a>
+			<h3><a href="/registration/form-a-team/">Compete</a></h3>
+			<div class="hline"></div>
+			<p>Find out if your high school has a team, if not, let’s start one together! Information on registration, competitions, &amp; team setup.</p>
+		</div><!-- end of col -->
+	</div><!-- end of four-columns -->
+
 <?php get_footer(); ?>
